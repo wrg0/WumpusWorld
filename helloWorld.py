736b93a5f3103604fd5288f9c69ef69af247cb9f
Totaram Ramratta <tramrattan@forbes.com>
2016-04-23 22:29:36 -0400

    init project

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+
+class speak():
+    def _init_(self,master=None):
+        master='this'
+
+    def sayHello(self):
+        print 'hello world'
+
+
+run = speak()
+run.sayHello()
---
+++ tk_example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import Tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def sayHello(self):
+        print 'hello wumpus world'
+
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit',
+            command= self.quit )
+        self.quitButton.grid()
+        self.startButton = tk.Button(self, text='start',
+            command=self.sayHello )
+        self.startButton.grid()
+
+app = Application()
+app.master.title('Wumpus World')
+app.mainloop()
